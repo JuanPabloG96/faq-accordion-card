6c7291c6017f2ea1f08f53eaee0f687a5f8bf631
Juan Pablo Gama Olivares <juanpablo.gama.olivares@gmail.com>
2024-07-02 09:14:00 -0600

    update: tailwind astro config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'very-dark-desaturated-blue': 'hsl(238, 29%, 16%)',
+				'soft-red': 'hsl(14, 88%, 65%)',
+				'very-dark-grayish-blue': 'hsl(237, 12%, 33%)',
+				'dark-grayish-blue': 'hsl(240, 6%, 50%)',
+				'soft-violet': 'hsl(273, 75%, 66%)',
+				'soft-blue': 'hsl(240, 73%, 65%)',
+				'dividers': 'hsl(240, 5%, 91%)'
+			}
+		},
 	},
 	plugins: [],
 }
